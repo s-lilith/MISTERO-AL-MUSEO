4dcc36efd86482c45bfb052e630b89739767eb87
Sharone <60832602+s-lilith@users.noreply.github.com>
2020-11-23 11:49:06 +0100

    Add files via upload

--- ambienteValutatore/versione 3/app.js
+++ ambienteValutatore/versione 3/app.js
@@ -1,26 +1,42 @@
 // SELECTORS
 const punteggi=document.querySelector(".punteggi-btn");
+var nomiCinni=["anna", "paolo", "elisa"];
+var points=[5 , 10, 20];
 
 // EVENT LISTENERS
 punteggi.addEventListener("click", getPunteggi);
 
+//BACK HOME
+
+
 // CONTROLLO PUNTEGGI
 function getPunteggi(){
-    document.write('<h1 color=red> PUNTEGGIO GIOCATORI</h1>')
-    document.write('<table border=1 cellpadding=10>');
-    for(i=1;i<=3;i++){
-        document.write('<tr>');
-        for(j=1;j<=4;j++){
-            document.write('<td>');
-            // document.write('a<sub>'+i+','+j+'</sub>');
-            document.write('</td>');
-        }
-        document.write('</tr>');
-    }
-    document.write('</table>');
-    document.write('<button onclick="index.html">back to home</button>');
-    document.close();
+    document.write('<table cellpadding=20 width=70% >');
+    
+    document.write('<thead>');
+    document.write('<tr>' + '<th>' + "Nome" +'</th>' + '<th>' + "Punteggio" +'</th>' + '<tr>');
+    document.write('</thead>');
+    //aggiungere colonna per nomi
+  for (let i=0; i<=nomiCinni.length-1; i++) { //modificare i<=10 con il numero di righe, ovvero il numero di giocatori
+    document.write('<tr>');
+    document.write('<td>' + nomiCinni[i] + '</td>');
+    document.write('<td>' + points[i] + '</td>');
+    document.write('</tr>');
+  }
+  var btnBack=document.createElement("button");
+  var btnText=document.createTextNode("Go Back");
+  btnBack.appendChild(btnText);
+  document.body.appendChild(btnBack);
+  $(btnBack).click(function(){
+      location.reload()
+  })
+  
 }
 
 
- 
+
+
+
+
+
+
